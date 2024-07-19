84ce70c9336554b1093f3f9dd18d12d8b265957e
Kreyno93 <michael.wagener@neuefische.de>
2024-07-19 15:22:02 +0200

    adding custom security group, for ssh and http

--- sg.tf
+++ sg.tf
@@ -6,22 +6,22 @@ resource "aws_security_group" "deham14-sg" {
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-    }
+  }
 
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    tags = {
-        Name = "WebServerSG"
-    }
+  tags = {
+    Name = "WebServerSG"
+  }
 }
